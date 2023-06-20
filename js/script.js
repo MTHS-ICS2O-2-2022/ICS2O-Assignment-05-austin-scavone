632edbae33247d81d8964616df29ec4892b7475e
austin <124389297+austin-scavone@users.noreply.github.com>
2023-06-20 00:41:31 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,42 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+/* Created by: austin scavone
+   Created on: june 2022
+   This file contains the JS functions for index.html*/
+
+'use strict'
+
+/**
+ * Check service worker.
+ */
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ASSIGNMENT-05-HTML/sw.js", {
+    scope: "/ASSIGNMENT-05-HTML/",
+  })
+}
+
+/**
+ * Input.
+ */
+
+function buttonClicked() {
+
+  var integer = document.getElementById("integer").value
+  var currentSymbol = "negative"
+  var bottomNumber = 1
+  var answer = (4 / bottomNumber)
+
+  if (integer > 1) {
+    for (let counter = 1; counter < integer; counter++) {
+      if (currentSymbol == "negative") {
+        bottomNumber = bottomNumber + 2
+        answer = answer - (4 / bottomNumber)
+        currentSymbol = "positive"
+      } else if (currentSymbol == "positive") {
+        bottomNumber = bottomNumber + 2
+        answer = answer + (4 / bottomNumber)
+        currentSymbol = "negative"
+      }
+    }
+  }
+  document.getElementById("output").innerHTML = answer.toFixed(10)
+  console.log(answer)
+}
